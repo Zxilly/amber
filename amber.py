083f98e696970b8dc863470c7aa9789486413024
Zxilly <zhouxinyu1001@gmail.com>
2021-08-02 15:04:11 +0800

    feat: inital implement

---
+++ amber.py
@@ -0,0 +1,38 @@
+from usp.tree import sitemap_tree_for_homepage
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import sys
+import requests
+
+
+def archive_request(url: str):
+    print(f"request {url}")
+    requests.post(f"https://web.archive.org/save/{url}", data={
+        'url': url,
+        'capture_all': 'on'
+    })
+
+
+def archive(target: str):
+    tree = sitemap_tree_for_homepage(target)
+
+    urls = set()
+
+    for page in tree.all_pages():
+        urls.add(page.url)
+
+    with ThreadPoolExecutor(max_workers=8) as t:
+        result_list = []
+        for url in urls:
+            result = t.submit(archive_request, url)
+            result_list.append(result)
+
+        as_completed(result_list)
+    print('archived')
+
+
+if __name__ == '__main__':
+    try:
+        target = sys.argv[1]
+        archive(target)
+    except IndexError:
+        print('Plz use url of your site as argument.')
